77c6db8b41d73623c75494665c7f3a2c3ef8fd3d
valentin28xd <123767550+valentin28xd@users.noreply.github.com>
2023-10-16 19:13:26 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by valentin student on 16.10.2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    return 0;
+}
