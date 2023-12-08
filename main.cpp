285fa4d8a4cfbb374e0c2aed2c3dd24f0d5623c7
valentin28xd <123767550+valentin28xd@users.noreply.github.com>
2023-12-08 19:05:46 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,24 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!\n";
-    cout << "Is it working?\n";
+int main()
+{
+    int nr;
+    int i, j;
+
+    cout << "Introduceti numarul: ";
+    cin >> nr;
+
+    for (i = 1; i < nr; i++) {
+        for (j = 1; j < nr; j++) {
+            if (i*i + j*j == nr) {
+                cout << "Numarul poate fi decompus in patratele: " << i << " " << j;
+                return 0;
+            }
+        }
+    }
+
+    cout << "Numarul nu poate fi decompus\n";
+
     return 0;
 }
